cdc9683bb8146114884f44e2e533c66d89af5766
Rishabh <rish.jaiswal100@gmail.com>
2023-04-09 19:56:19 +0530

    completed the form 9APR 19:56PM

---
+++ assets/form.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+
+    </header>
+    <section class="main-form">
+        <div class="personal-details">
+            <div class="pd-head">
+                <p>Personal details</p>
+            </div>
+            <div class="pd-details">
+                <form action="" method="post">
+                    <div class="u-name">
+                        <div class="f-cont">
+                            <label for="name">Name</label>
+                            <input type="text" placeholder="Student full name">
+                        </div>
+
+                        <div class="f-cont">
+                            <label for="fname">Father's name</label>
+                            <input type="text" placeholder="Father's full name">
+                        </div>
+                    </div>
+
+                    <div class="u-p-ad">
+                        <div class="f-cont">
+                            <label for="p-add">Permanent address</label>
+                            <input type="text" placeholder="If you are an outsider then enter your native address">
+                        </div>
+
+                        <label for="p-mob">Phone No</label>
+                        <input type="number" placeholder="999-999-9999">
+                    </div>
+
+                    <div class="u-l-ad">
+                        <label for="l-add">Local address</label>
+                        <input type="text" placeholder="Enter you current residential address">
+
+                        <label for="l-mob">Phone No</label>
+                        <input type="number" placeholder="999-999-9999">
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="academic-details">
+
+        </div>
+
+        <div class="subject">
+
+        </div>
+    </section>
+    <footer>
+
+    </footer>
+</body>
+</html>
